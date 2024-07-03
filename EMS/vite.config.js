e2b6a42c74df678c6d5c65ffa6fad527c979120b
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-07-03 22:30:26 +0530

    last

--- EMS/vite.config.js
+++ EMS/vite.config.js
@@ -12,9 +12,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 8001,
+    outDir: 'dist'
   }
 });
