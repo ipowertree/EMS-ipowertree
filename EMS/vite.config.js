8167958bd372714f16afeebdcc810ec84e2e64fd
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-07-05 11:10:01 +0530

    final

--- EMS/vite.config.js
+++ EMS/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
-
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/public/index.html',
+    }
+  }
+});
